0e0eb4150a9fbcfde0fcb3a58f050fb7461b1220
Vadim <71722806+VadimBolotin@users.noreply.github.com>
2022-12-10 14:32:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ $(function(){
     slidesToShow: 1,
     slidesToScroll: 1,
     asNavFor: '.herous__slider-text',
-    prevArrow:'<button class="slick-btn slick-prev"><img src="../images/arrow-left.png" alt=""></button>',
-    nextArrow:'<button class="slick-btn slick-next"><img src="../images/arrow-right.png" alt=""></button>',
+    prevArrow:'<button class="slick-btn slick-prev"><img src="./images/arrow-left.png" alt=""></button>',
+    nextArrow:'<button class="slick-btn slick-next"><img src="./images/arrow-right.png" alt=""></button>',
     responsive: [
       {
         breakpoint: 769,
